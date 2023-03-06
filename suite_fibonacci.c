b98ef18c16e857882375c77e4cd29b37aaca6381
LouNuX <80525870+LouNuX@users.noreply.github.com>
2023-03-06 12:44:38 +0100

    Add files via upload
    Intro_C_lundi_27_02_2023

---
+++ suite_fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fibonacci(int max_fib){
+    int ret_fib;
+    int f0=0;
+    int f1=1;
+    
+    while(max_fib > 1){
+        ret_fib=f0+f1;
+        f0=f1;
+        f1=ret_fib;
+        max_fib--;
+
+    }
+    return (ret_fib);
+}
+
+int main(void){
+    int fib;
+    fib = fibonacci(7);
+    printf("%i\n", fib);
+    return(1);
+}
+
