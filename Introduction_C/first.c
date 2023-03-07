56f45116346b5677283f5af5439a103aa733644c
LouNuX <80525870+LouNuX@users.noreply.github.com>
2023-03-07 18:47:47 +0100

    Introduction_C_functions

---
+++ Introduction_C/first.c
@@ -0,0 +1,15 @@
+int loop(int max_int){
+    int i;
+    i=0;
+    while (max_int > 0){
+        i= i+2;
+        max_int--;
+    }
+    return(i);
+}
+
+int main(void){
+    int my_return;
+    printf("%i\n, my_return");
+    return(my_return);
+}
